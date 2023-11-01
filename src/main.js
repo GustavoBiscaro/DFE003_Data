48d49aa51d55db3cb7494c46b50809011432f801
Gustavo Miguel Oliveira Biscaro <gustavo.biscaro@gmail.com>
2023-10-31 21:04:18 -0300

    albuns voice

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // Importe o arquivo JSON com a afirmação de importação
 import beatles from './data/beatles_songs/beatles_songs.json' assert {type: "json"};
 
+
 const totalDeAlbuns = document.getElementById("totalOfalbuns");
 const nomeDeAlbuns = document.getElementById("nameOfalbuns");
 const totalTracklist = document.getElementById("totalOftracklist");
 const listTracklist = document.getElementById("listaOftracklist");
 
 const beatlesObject = JSON.parse(JSON.stringify(beatles, null, 2));
+
 
 
 // Exibe a quantidade total de álbuns
@@ -60,7 +62,48 @@ function tracklist() {
 
 listTracklist.innerHTML = "<h2>Tracklist</h2>" + "<img src='./assets/div_img/tracklist.jpg' width='200'>" + tracklist();
 
+// vozes
+var txtInput = document.querySelector('#txtInput');
+var voiceList = document.querySelector('#voiceList'); //lista de voz
+var btnSpeak = document.querySelector('#btnSpeak');
+var synth = window.speechSynthesis;
+var voices = []
 
+newVoices()
+if(speechSynthesis !== undefined) {
+  speechSynthesis.onvoiceschanged = newVoices
+}
+
+btnSpeak.addEventListener('click', ()=> {
+  txtInput.value = tituloPorAlbum() + " e possuem um " + totalAlbuns();
+  var toSpeak = new SpeechSynthesisUtterance(txtInput.value)
+  var selectedVoiceName = voiceList.selectedOptions[0].getAttribute('data-name');
+  voices.forEach((voice) => {
+    if(voice.name === selectedVoiceName) {
+      toSpeak.voice = voice
+    }
+  })
+
+  synth.speak(toSpeak)
+})
+
+
+function newVoices(){
+  voices = synth.getVoices()
+  // caso não haja valor retorna zero, se verdadeiro vasculha a lista
+  var selectedIndex = voiceList.selectedIndex < 0 ? 0 : voiceList.selectedIndex
+  voiceList.innerHTML = ''
+  voices.forEach((voice) =>{
+    var listItem = document.createElement('option')
+    listItem.textContent = voice.name
+    listItem.setAttribute('data-lang', voice.lang)
+    listItem.setAttribute('data-name', voice.name)
+    voiceList.appendChild(listItem)
+  })
+
+  voiceList.selectedIndex = selectedIndex
+
+}
 
 
 
